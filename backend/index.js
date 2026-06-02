2b9b9b837d679eb811e579ef0ea5f64b211624be
Adham-0110 <mailinadham@gmail.com>
2026-06-02 13:43:43 +0530

    Change MongoDB connection credentials
    Updated MongoDB connection string with new credentials.

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(cors())
 
 
-mongoose.connect("mongodb+srv://mailinadham:88GMbFP19KHYiXgO@cluster0.cipq4mk.mongodb.net/passkey?retryWrites=true&w=majority&appName=Cluster0").then(function(){
+mongoose.connect("mongodb+srv://mailinadham:zH9wwwQn4eWUEkAo@cluster0.cipq4mk.mongodb.net/passkey?retryWrites=true&w=majority&appName=Cluster0").then(function(){
     console.log("Connected to Db")
 }).catch(function(){
     console.log("Failed to connect DB")
