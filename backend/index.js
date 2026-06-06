cc7c3dbca4036bf5b67bdd0a9a280d598ea21e57
Adham-0110 <mailinadham@gmail.com>
2026-06-06 17:27:58 +0530

    fix credentials

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(cors())
 
 
-mongoose.connect("mongodb+srv://mailinadham_db_user:WzjWLGFxHH3LpnZz@cluster0.x7op6na.mongodb.net/?appName=Cluster0").then(function(){
+mongoose.connect("mongodb+srv://mailinadham_db_user:WzjWLGFxHH3LpnZz@cluster0.x7op6na.mongodb.net/passkey?appName=Cluster0").then(function(){
     console.log("Connected to Db")
 }).catch(function(){
     console.log("Failed to connect DB")
@@ -23,14 +23,16 @@ app.post("/sendmail",function(req,res){
     let emailList = req.body.emailList
   
     credential.find().then(function(data){
+      
         const transporter = nodemailer.createTransport({
             service:"gmail",
             auth: {
               user: data[0].toJSON().user,
               pass: data[0].toJSON().pass,
             },
+            
           });
-    
+            
           new Promise(async function(resolve,reject){
             try{ 
                 for(let i=0;i<emailList.length;i++){
@@ -62,7 +64,6 @@ app.post("/sendmail",function(req,res){
  
 
 })
-
 
 
 
